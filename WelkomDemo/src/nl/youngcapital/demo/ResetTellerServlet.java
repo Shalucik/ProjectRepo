ba0993207b43a073be0be14bb15f99165582e88b
Wouter Remijn <wouter.remijn@gmail.com>
2017-06-16 10:52:26 +0200

    Created Servlet to track how many people found the servlet Created Servlet to reset previoes tracker

---
+++ WelkomDemo/src/nl/youngcapital/demo/ResetTellerServlet.java
@@ -0,0 +1,25 @@
+package nl.youngcapital.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ResetTellerServlet
+ */
+@WebServlet("/Reset")
+public class ResetTellerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		TellerServlet.setCounter(0);
+		response.getWriter().print(TellerServlet.getCounter());
+	}
+
+}
