7ccbfb286d83db7356aa1f585e46ded58c577a56
Wouter Remijn <wouter.remijn@gmail.com>
2017-06-16 11:25:52 +0200

    Changed redirect to redirect to the new loop servlet that loops back to redirect

--- WelkomDemo/src/nl/youngcapital/demo/LoopServlet.java
+++ WelkomDemo/src/nl/youngcapital/demo/LoopServlet.java
@@ -8,17 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class RedirectServlet
+ * Servlet implementation class LoopServlet
  */
-@WebServlet("/WrongPlaceWrongTime")
-public class RedirectServlet extends HttpServlet {
+@WebServlet("/Loop")
+public class LoopServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect(request.getContextPath() + "/Loop");
+		response.sendRedirect(request.getContextPath() + "/WrongPlaceWrongTime");
 	}
+
 
 }
