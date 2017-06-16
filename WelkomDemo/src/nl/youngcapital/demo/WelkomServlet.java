82e683e24278d2871d5f79f0cfa40548a3102e4b
Wouter Remijn <wouter.remijn@gmail.com>
2017-06-16 10:51:26 +0200

    Created Servlet worth 47,000 dollars

---
+++ WelkomDemo/src/nl/youngcapital/demo/WelkomServlet.java
@@ -0,0 +1,28 @@
+package nl.youngcapital.demo;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelkomServlet
+ */
+@WebServlet("/Money")
+public class WelkomServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+          	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String output = "Left";
+		Random random = new Random();
+		if(random.nextBoolean())
+			output = "Right";
+		response.getWriter().print(output);
+	}
+
+
+}
