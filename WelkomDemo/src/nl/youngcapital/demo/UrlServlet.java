9ef4ea92ecd2e7da80665754651f3015411d99ad
Wouter Remijn <wouter.remijn@gmail.com>
2017-06-16 10:52:38 +0200

    Created servlet to return the url

---
+++ WelkomDemo/src/nl/youngcapital/demo/UrlServlet.java
@@ -0,0 +1,25 @@
+package nl.youngcapital.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UrlServlet
+ */
+@WebServlet("/Url/*")
+public class UrlServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = request.getRequestURI();
+		response.getWriter().print(url);
+	}
+
+}
